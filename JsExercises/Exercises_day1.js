cf2d3288b664e68d733863be10e98076f825bcb1
Lucas Da Silva <lucasilva15@live.com>
2022-08-10 23:34:50 -0400

    JS Classwork 8/10/2022

---
+++ JsExercises/Exercises_day1.js
@@ -0,0 +1,145 @@
+//Exercise 1
+
+// let number1 = 2;
+// let number2 = 4;
+// let answer = number1 + number2;
+// console.log(answer);
+
+
+//Exercise 2
+
+// let number3 = 8;
+// let number4 = 9;
+// let number5 = 12;
+// let number6 = 32;
+// let answer2 = number3 + number4 / number5 * number6;
+
+// console.log(answer2);
+
+//Exercise 3
+
+// let number7 = 39;
+// let number8 = 49;
+// let number9 = 23;
+// let number10 = 12;
+// let number11 = 18;
+// let number12 = 2;
+// let number13 = 9;
+// let number14 = 26;
+// let number15 = 53;
+// let number16 = 44;
+
+
+// let average1 = (number7 + number8 + number9 + number10 + number11)/5;
+// let average2 = (number12 + number13 + number14 + number15+ number16)/5;
+// let average3 = (average1 + average2)/2;
+
+// console.log(average1);
+// console.log(average2);
+// console.log(average3);
+
+//Exercise 4
+
+// let avg1 = (4 + 10 + 22 - 30 + 55)/5;
+// let avg2 = (68 + 7 -22 + 9 + 100)/5;
+// let avg3 = (avg1 + avg2)/2;
+
+// console.log(avg1);
+// console.log(avg2);
+// console.log(avg3);
+
+//Bonus Exercise
+
+// let box1 = "class";
+// let box2 = "hello";
+// let box3 ;
+
+// box3 = box1;
+// box1 = box2;
+// box2= box3;
+// console.log(box1, box2)
+
+//Exercise 5 & 6
+
+// let num = -8
+
+// if (num > 0){
+//     console.log("number is positive")
+// } else if (num === 0) {
+//     console.log("this number is 0")
+// } else {
+//     console.log("this number is negative")
+// }
+
+// Exercise 7
+
+// let num2 = 17
+
+// if(num2 >= 18){
+//     console.log("You are old enough to view")
+// } else {
+//     console.log("you are not old enough to view")
+// }
+
+//Exercise 8
+
+// let num3 = -8;
+
+// if(num3 > 0){
+//     if (num3 > 100){
+//         console.log("this number is greater than 100")
+//     } else {
+//         console.log("this number is less than 100")
+//     }
+// } else {
+//     console.log("this number negative ")
+// }
+
+
+//Exercise 9
+
+// let grade = 54
+
+// if(grade >= 55){
+//     if (grade >= 90) {
+//         console.log("You got an A")
+//     } else if (grade >= 80) {
+//         console.log("You got a B")
+//     } else if (grade >= 70){
+//         console.log("You got a C")
+//     } else {
+//         console.log("you got a D")
+//     }
+// } else {
+//     console.log("You failed")
+// }
+
+//Exercise 10
+
+// let letter = "C"
+
+// switch(letter){
+// case "A":
+// case "E":
+// case "I":
+// case "O":
+// case "U":
+//     console.log("This letter is a vowel")
+//     break
+// default:
+//     console.log("This letter is a consonant")
+// 
+
+//Exercise 11
+
+// let number = -8
+// let answer = number === 0 ? "this number is 0" :
+// number > 0
+// ? "this number is positive" 
+// : "this number is negative" 
+// console.log(answer)
+
+
+
+    
+
