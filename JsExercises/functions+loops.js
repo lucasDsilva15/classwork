c6bfae3e63a2c6196937a7ba2e9a018d8b2d22cc
Lucas Da Silva <lucasilva15@live.com>
2022-08-11 22:10:57 -0400

    loops and functions work 8/11/22

---
+++ JsExercises/functions+loops.js
@@ -0,0 +1,116 @@
+//EXERCISE 1
+
+// function computearea (width, height){
+//     return "The area of a rectangle with a width of " + width + " and a height of " + height + " is " + width*height + " square units";
+// }
+// console.log(computearea(9, 3))
+
+//EXERCISE 2
+
+// const planetHasWater = function(planet){
+//     if (planet.toLowerCase() == "earth" || planet.toLowerCase() == "mars"){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// console.log(planetHasWater("EArth"))
+
+
+//BONUS EXERCISE 1
+
+// const computearea = (width, height) => "The area of a rectangle with a width of " + width + " and a height of " + height + " is " + width*height + " square units"
+// console.log(computearea(5, 6))
+
+// const planetHasWater = (planet) => planet.toLowerCase() === "earth" || planet.toLowerCase() === "mars" ? true : false
+// console.log(planetHasWater("zegorp"))
+
+//EXERCISE 3
+
+// for (let i = 10; i >= 0; i--){
+//     console.log(i)
+// }
+
+//EXERCISE 4
+// for (let i = 0; i <=20; i++){
+// if (i<10 && i%2!=0) {
+//     console.log(i);
+// }
+// if (i>10 && i%2==0) {
+//     console.log(i-10);
+// }
+// }
+
+//EXERCISE 5
+// for (let i = 6; i <= 60; i+=3){
+//     console.log(i)
+// }
+
+//EXERCISE 6
+// let text =""
+// for (i = 0; i <= 7; i++){
+//     console.log(text += "#")
+// }
+
+//EXERCISE 7
+
+// for (i = 0; i <= 10; i++){
+//     if (i %2 === 0){
+//     console.log(i);
+// } else {
+//     continue;
+// }
+// }
+
+// EXERCISE 8
+// for (i = 1; i <= 20; i++ ){
+//    if( i == 1 ){
+//    console.log(i ,"this number is odd")
+//    }
+//    else if( i == 2){
+//     console.log(i , "this number is even")
+//    } else {
+//     if (i %2!= 0 && i %3!= 0 ){
+//         console.log(i , "this number is prime and odd")
+//     } else if(i %2!= 0  ){
+//     console.log(i , "this number is odd")
+//     }
+//     if(i %2 === 0){
+//         console.log(i , "this number is even")
+//     }
+// }
+// }
+
+
+//EXERCISE 9
+// let num = 1
+// while (num <= 35){
+//     console.log(num)
+//     num += 3
+// }
+
+//EXERCISE 10
+// let num = 5
+// while (num <=100){
+//     console.log(num)
+//     num += 5
+// }
+
+//EXERCISE 11
+// let num = 0
+// while (num <= 20){
+//     if (num %2 == 0){
+//         console.log(num*3)
+//     }
+//     num ++
+// }
+
+//EXERCISE 12
+// let num = 1
+// while (num < 20 ){
+//    if(num %2!= 0 && num %3!= 0){
+//         console.log(num)
+//     }
+//     num ++
+// } 
+
