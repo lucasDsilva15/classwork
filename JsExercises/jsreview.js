13019c1d50e8f6aaeec105176337921619344d26
Lucas Da Silva <lucasilva15@live.com>
2022-08-18 15:33:16 -0400

    Second push for review

--- JsExercises/jsreview.js
+++ JsExercises/jsreview.js
@@ -1,4 +1,6 @@
-// A. Q + A
+// SECTION 1 VARIABLES AND DATA TYPES
+
+//A. Q + A
 
 //1. An assignment opperator ( Let,const,var = (your variable) )
 
@@ -52,10 +54,10 @@
 //3. ==
 //4. ||
 //5. && x 4 || (at the end)
-//6. &&
+//6. ==
 //7. ===
-//8. < , &&
-//9. == , !==
+//8. < , && or + , ===
+//9. * , ===
 //10. ==
 
 
@@ -77,7 +79,89 @@
 //     console.log("Hey! your not a cow")
 // }
 
-//4. 
+//4. pushed
+
+
+// Review E
+
+//1.
+// let age = 15
+
+//2.
+// if (age >= 16){
+//     console.log("Here are the keys")
+// } else if (age < 16){
+//     console.log("sorry your too young")
+// }
+
+
+
+// SECTION 2 LOOPS
+// A The Basics
+
+//1. 
+// for (let i = 0; i <= 10; i++){
+//     console.log(i)
+// }
+
+//2. 
+// for (let i = 10; i <= 400; i++){
+//     console.log(i);
+// }
+
+//3. 
+// for (let i = 12; i <= 4000; i+=3){
+//     console.log(i)
+// }
+
+
+//B. Get Even
+
+// 1. 
+// for (let i = 1; i <= 100; i++){
+//         console.log(i);
+//}
+
+//2.
+// for (let i = 1; i <= 100; i++){
+//      if (i %2 == 0){
+//     console.log(i + "<-- this number is even");
+// } else {
+//     console.log(i)
+// }
+// }
+
+
+//C. Give me Five
+
+//1
+// for (let i = 0; i <= 100; i++){
+//      if (i %5 == 0){
+//     console.log(" I found a " + i + " High five");
+// } 
+// }
+
+//2
+// for (let i = 0; i <= 100; i++){
+//     if (i %5 == 0 && i %3 == 0){
+//    console.log(" I found a " + i + " High five!");
+//    console.log("I found a " + i + " Three is a crowd")
+// } else if (i %3 == 0){
+//    console.log("I found a " + i + " Three is a crowd")
+// }
+// }
+
+
+//D. Savings Account
+let sum = 0
+for (i = 1; i <= 10; i++){
+    sum =+ i
+}
+console.log(sum)
+
+
+
+
 
 
 
